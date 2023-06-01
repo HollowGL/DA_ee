bb0ecef7a24f0abf69335f5e0fbef8ed6628df30
guil <guil21@mails.tsinghua.edu.cn>
2023-06-01 21:38:07 +0800

    oj9 ok!

--- 11.cpp
+++ 11.cpp
@@ -2,15 +2,16 @@
 #include<cmath>
 #include<algorithm>
 
+bool debug = false;
+
 struct point {
-    int x, y;
+    double x, y;
     bool operator==(point p) const { return x == p.x && y == p.y; }
 };
 
 bool cmp(point a, point b) {
-    if (a.y == b.y && a.x < b.x) return true;
-    else if (a.y < b.y) return true;
-    return false;
+    if (a.y == b.y) return a.x < b.x;
+    return a.y < b.y;
 }
 
 bool xcross(point a, point b, point c) {
@@ -30,28 +31,43 @@ point dedup[100005];
 
 
 int main() {
+    if (debug) freopen("./data.in", "r", stdin);
+
     int n0, n, total;
     scanf("%d", &total);
 
     for (int cur = 0; cur < total; ++cur) {
         scanf("%d", &n0);
         for (int i = 0; i < n0; ++i) {
-            scanf("%d%d", &node[i].x, &node[i].y);
+            scanf("%lf%lf", &node[i].x, &node[i].y);
         }
-        std::sort(node,node + n0,cmp);
+        std::sort(node, node + n0, cmp);
+
+        if (debug) {
+            printf("排序之后：n0 = %d\n", n0);
+            for (int i = 0; i < n0; ++i) {
+                printf("(%d, %d) ", node[i].x, node[i].y);
+            }
+            printf("\n");
+        }
 
         // 去重
         dedup[0].x = node[0].x;
         dedup[0].y = node[0].y;
         n = 1;
         for (int i = 1; i < n0; ++i) {
-            if (node[i] == node[i - 1]) {
-                i++;
-                if (i == n0) break;
-            }
+            if (node[i] == node[i - 1]) continue;
             dedup[n].x = node[i].x;
             dedup[n].y = node[i].y;
             n++;
+        }
+
+        if (debug) {
+            printf("去重之后：n = %d\n", n);
+            for (int i = 0; i < n; ++i) {
+                printf("(%d, %d) ", dedup[i].x, dedup[i].y);
+            }
+            printf("\n");
         }
 
         if (n == 1 || n == 2) {
@@ -59,7 +75,8 @@ int main() {
             continue;
         }
 
-        num[0]=0; num[1]=1;
+        num[0] = 0; 
+        num[1] = 1;
         int top = 1;
         for (int i = 2; i < n; ++i) {
             while (top > 0 && xcross(dedup[i], dedup[num[top]], dedup[num[top - 1]]))
@@ -75,19 +92,27 @@ int main() {
             num[top] = i;
         }
 
+        if (debug) {
+            printf("计算凸包：top = %d\n", top);
+            for (int i = 0; i < top; ++i) {
+                printf("(%d, %d) ", dedup[num[i]].x, dedup[num[i]].y);
+            }
+            printf("\n");
+            printf("\n");
+        }
+
         if (top == 1 || top == 2) {
             printf("0.0000000\n");
             continue;
         }
 
-        num[top] = num[0];
         num[top + 1] = num[1];
         double max = -1;
         for (int i = 0; i < top; ++i) {
             double temp = angle(dedup[num[i]], dedup[num[i + 1]], dedup[num[i + 2]]);
             if (temp > max) max = temp;
         }
-        printf("%.7lf\n", acos(max) * 180 / M_PI);
+        printf("%.7lf\n", acos(max) * (180 / acos(-1)));
     }
 
     return 0;
